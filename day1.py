455380b50c782336be06725cc3695464af915a83
gopi-devops-cloud <gopidon1m@gmail.com>
2026-01-11 15:42:27 +0530

    day 1

--- day1.py
+++ day1.py
@@ -1,14 +1,15 @@
-# print("hello world!\nhello world\nhello world")
-# print("gopi" +" "+ "bhatt")
-# print("hello "+ input("what is your name?")+"!")
-# #comments
-# #len function
-# print(len(input("what is your name? ")))
-# username=input("what is your name? ")
-# length=len(username)
-# print(length)
+print("hello world!\nhello world\nhello world")
+print("gopi" +" "+ "bhatt")
+print("hello "+ input("what is your name?")+"!")
+#comments
+#len function
+print(len(input("what is your name? ")))
+username=input("what is your name? ")
+length=len(username)
+print(length)
 # project day 1
 print("welcome to the band name Generator")
 city=input("what is your city name?\n ")
 pet=input("what's your pet name? \n")
 print("your band name could be:\n" +city+" "+pet)
+
