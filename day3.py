16971e839b3bde95ce790c938d4727c272dd8312
gopi-devops-cloud <gopidon1m@gmail.com>
2026-01-12 19:43:18 +0530

    day 3

--- day3.py
+++ day3.py
@@ -23,35 +23,35 @@ else :
  print("sorry you can't ride ")
 
 
-#     # module operator check odd and even
-# num=int(input("enter a number"))
-# if num%2==0:
-#     print("even")
-# else:
-#     print("odd")
+    # module operator check odd and even
+num=int(input("enter a number"))
+if num%2==0:
+    print("even")
+else:
+    print("odd")
 
 
 
-# #pizza yummy
-# print("welcome to the pizza deliveries!")
-# size=input("what size do you want? S, M or L: ")
-# bill=0
-# if size == "S":
-#  bill=15
-# elif size=="M":
-#  bill=20
-# else:
-#  bill=25
-# pepperoni=input("do you want pepperoni for your pizza y for yes n for no ") 
-# if pepperoni== "y":
-#     if size=="S":
-#      bill+=2
-#     else:
-#       bill+=3
-# extra_cheese =input("do you want extra_cheese y for yes n for no ")
-# if extra_cheese=="y":
-#  bill+=1
-# print(f" your total bill is ${bill}")
+#pizza yummy
+print("welcome to the pizza deliveries!")
+size=input("what size do you want? S, M or L: ")
+bill=0
+if size == "S":
+ bill=15
+elif size=="M":
+ bill=20
+else:
+ bill=25
+pepperoni=input("do you want pepperoni for your pizza y for yes n for no ") 
+if pepperoni== "y":
+    if size=="S":
+     bill+=2
+    else:
+      bill+=3
+extra_cheese =input("do you want extra_cheese y for yes n for no ")
+if extra_cheese=="y":
+ bill+=1
+print(f" your total bill is ${bill}")
 
 
     
