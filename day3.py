b4fef757ca147fdc251d9041b86a0b1bde9048a6
gopi-devops-cloud <gopidon1m@gmail.com>
2026-01-12 09:46:27 +0530

    day 3

---
+++ day3.py
@@ -0,0 +1,22 @@
+print("welcome to the rollercoaster!")
+hight=int(input("what is your hight in cm? "))
+if hight>=120:
+    print("you can ride")
+    age=int(input("enter a age"))
+    if age<=12:
+     print("please pay $5")
+    elif age <= 18:
+     print("please pay $7")
+    else :
+     print("please pay $12")
+else :
+ print("sorry you can't ride ")
+    # module operator check odd and even
+num=int(input("enter a number"))
+if num%2==0:
+    print("even")
+else:
+    print("odd")
+  #  day 3
+
+    
