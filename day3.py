992e0750e37a332dbed3c05fe6be6a2b7080ab00
gopi-devops-cloud <gopidon1m@gmail.com>
2026-01-13 08:27:23 +0530

    day 3

--- day3.py
+++ day3.py
@@ -1,57 +1,75 @@
-print("welcome to the rollercoaster!")
-hight=int(input("what is your hight in cm? "))
-bill=0
-if hight>=120:
-    print("you can ride")
-    age=int(input("enter a age "))
-    if age<=12:
-     bill=5
-     print("child tickets are $5")
-    elif age <= 18:
-     bill=7
-     print("youth tickets are $7")
-    elif  age >=45 & age<=55:
-     print("free ride for you senior" )
-    else :
-     bill=12
-     print("adults ticket are $12")
-    wants_photo=input("do you want to take a photo y for yes n for no ")
-    if wants_photo == "y":
-       bill+=3
-    print(f"your final bill is ${bill}")  
-else :
- print("sorry you can't ride ")
+# print("welcome to the rollercoaster!")
+# hight=int(input("what is your hight in cm? "))
+# bill=0
+# if hight>=120:
+#     print("you can ride")
+#     age=int(input("enter a age "))
+#     if age<=12:
+#      bill=5
+#      print("child tickets are $5")
+#     elif age <= 18:
+#      bill=7
+#      print("youth tickets are $7")
+#     elif  age >=45 & age<=55:
+#      print("free ride for you senior" )
+#     else :
+#      bill=12
+#      print("adults ticket are $12")
+#     wants_photo=input("do you want to take a photo y for yes n for no ")
+#     if wants_photo == "y":
+#        bill+=3
+#     print(f"your final bill is ${bill}")  
+# else :
+#  print("sorry you can't ride ")
 
 
-    # module operator check odd and even
-num=int(input("enter a number"))
-if num%2==0:
-    print("even")
+#     # module operator check odd and even
+# num=int(input("enter a number"))
+# if num%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+
+# #pizza yummy
+# print("welcome to the pizza deliveries!")
+# size=input("what size do you want? S, M or L: ")
+# bill=0
+# if size == "S":
+#  bill=15
+# elif size=="M":
+#  bill=20
+# else:
+#  bill=25
+# pepperoni=input("do you want pepperoni for your pizza y for yes n for no ") 
+# if pepperoni== "y":
+#     if size=="S":
+#      bill+=2
+#     else:
+#       bill+=3
+# extra_cheese =input("do you want extra_cheese y for yes n for no ")
+# if extra_cheese=="y":
+#  bill+=1
+# print(f" your total bill is ${bill}")
+
+# day 3 project
+print("welcome to the treasure island ")
+print("your mission is to find the treasure")
+choice_1=input('you\'re at a crossroad, where do you want to go "left" and "right ".').lower()
+if choice_1 == "right":
+ print("game over you are hit by car lol")
 else:
-    print("odd")
-
-
-
-#pizza yummy
-print("welcome to the pizza deliveries!")
-size=input("what size do you want? S, M or L: ")
-bill=0
-if size == "S":
- bill=15
-elif size=="M":
- bill=20
-else:
- bill=25
-pepperoni=input("do you want pepperoni for your pizza y for yes n for no ") 
-if pepperoni== "y":
-    if size=="S":
-     bill+=2
-    else:
-      bill+=3
-extra_cheese =input("do you want extra_cheese y for yes n for no ")
-if extra_cheese=="y":
- bill+=1
-print(f" your total bill is ${bill}")
-
-
-    
+ choice_2 = input('you are at a lake now go to island for trasure do you want to"swim" or "wait "').lower()
+ if choice_2 == "swim":
+    print("game over you are drowned by exhaustion ")
+ else:
+       choice_3= input("now you are very close choose carefully which dooor you want to go ? red,yellow,blue ").lower()
+       if choice_3=="red":
+         print("room full of bugs game over")
+       elif choice_3=="blue":
+         print("room full of trex you died game over") 
+       elif choice_3== "yellow":
+         print("you succesfully find a biggest treasure ever one doller")
+       else:
+        print("can't read? a wrong choice ")    
