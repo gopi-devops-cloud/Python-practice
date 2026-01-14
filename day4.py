6b32c93622afad646eb5503cb00176bc9c37ad7c
gopi-devops-cloud <gopidon1m@gmail.com>
2026-01-14 09:08:57 +0530

    day 4

---
+++ day4.py
@@ -0,0 +1,24 @@
+# import random
+# random_integer = random.randint(1,10)
+# print(random_integer)
+# # 1 & 10 both included
+
+# float number
+# random_number_0_to_1= random.random()
+# print(random_number_0_to_1) 
+#include o but not 1
+
+# random_float=random.uniform(1,10)
+# print(random_float)
+ 
+# random_integar=random.randint(1,2)
+# if random_integar==1:
+#  print("heads")
+# else:
+#  print("tails")
+
+fruits=(["cherry","apple","pear","orange"])
+fruits.append(["mango","orange"])
+fruits[0]="banana"
+print (fruits[0])
+print(fruits)
