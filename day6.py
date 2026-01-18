94b540515c47a265edbb20624b8e2e166bd6c414
gopi-devops-cloud <gopidon1m@gmail.com>
2026-01-18 10:59:05 +0530

    day 6

--- day6.py
+++ day6.py
@@ -73,3 +73,63 @@
 # jump()
 # jump()
 # jump() 
+
+# #while loop
+# def turn_right ():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# while not at_goal():
+#     jump()
+
+# # hurdle 3
+# def turn_right ():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()  
+
+# hurdle 4
+# def turn_right ():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     turn_left()
+#     while not right_is_clear():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while not wall_in_front():
+#         move() 
+#     turn_left()   
+    
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()  
