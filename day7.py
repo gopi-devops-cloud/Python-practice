415d6798973b66a81c5c7555fb1c96dfaf4517b3
gopi-devops-cloud <gopidon1m@gmail.com>
2026-01-22 07:32:33 +0530

    day 7

--- day7.py
+++ day7.py
@@ -1,7 +1,7 @@
 import random
 word_list=["gopi","shivam","arun","adi","rohit"]
+lives=(6)
 choosen_word=random.choice(word_list)
-print(choosen_word)
 placeholder=""
 word_length=len(choosen_word)
 for position in range(word_length):
@@ -9,8 +9,12 @@ for position in range(word_length):
 print(placeholder) 
 game_over=False
 correct_letters=[]
-while not game_over :    
+while not game_over :   
+    print(f"{lives}/6 lives remaining") 
     user_guess=(input("guess a letter?\n")).lower()
+    if user_guess in correct_letters:
+       print(f"you already guessed {user_guess}")
+       
     display = ""
     for letter in choosen_word:
         if letter == user_guess:
@@ -21,6 +25,12 @@ while not game_over :
         else:
          display += "_"
     print(display)  
+    if user_guess not in choosen_word:
+       lives-=1
+       print(f"you guessed {user_guess},is not in the word you lose a life.")
+       if lives==0:
+          game_over=True
+          print(f"It was {choosen_word} you lose")
     if "_" not in display:
        game_over=True
        print("you win")
