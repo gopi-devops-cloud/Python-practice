13ac6f1116b82be31df3635aca52139f69f84e25
gopi-devops-cloud <gopidon1m@gmail.com>
2026-03-06 08:17:16 +0530

    day 10

---
+++ day10.py
@@ -0,0 +1,10 @@
+a=float(input(" Enter students marks:"))
+if a>=90:
+    print("you got grade A")
+elif a>=80:
+    print("you got grade B")
+elif a>=70:
+    print("you got grade c")
+elif a<70:
+    print("you got grade d")
+
